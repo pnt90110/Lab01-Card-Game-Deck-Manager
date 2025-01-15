09e9960537ba390d7c330f08f56608a424c4dc61
pnt90110 <panta90111@hotmail.com>
2025-01-15 12:28:26 +0700

    Add Rectangle class

---
+++ src/rect/Rectangle.java
@@ -0,0 +1,23 @@
+package rect;
+
+public class Rectangle {
+
+	private int width ,height;
+	
+	public Rectangle(int width, int height) {
+		super();
+		this.width = width;
+		this.height = height;
+	
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	
+}
